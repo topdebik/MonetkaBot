26689cb80a89bb5b9c01464e9a50fa2b6857770a
topdebik <sashasemichastnov@gmail.com>
2024-11-14 15:21:43 +0300

    add openvpn wrapper in order to get Gemini working from not allowed regions

--- gemini_fetcher.py
+++ gemini_fetcher.py
@@ -1,14 +1,20 @@
 import google.generativeai as genai
+from openvpnclient import OpenVPNClient  # thanks https://github.com/KTH-SSAS/python-openvpn-client
 import asyncio
 import consts
 
 genai.configure(api_key=consts.GEMINI_TOKEN)
+vpn = OpenVPNClient(consts.vpn_file_path)
 
 
 async def get_answer_from_gemini(prompt):
+    vpn.connect()
+
     model = genai.GenerativeModel("gemini-1.5-flash")
     await asyncio.sleep(0)
     response = model.generate_content(prompt)
+    
+    vpn.disconnect()
     return response.text
 
 
